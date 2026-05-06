ed27a9cc5dfcdf56faa95cd3e6c84e0465125834
Yway Chit Aung <ywaychitaung@gmail.com>
2026-05-06 19:56:54 +0800

    Fix issue

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,7 +1,5 @@
-// Create this file if it doesn't exist
 const config = {
-    // Make sure this URL matches your backend API
-    API_Base_URL: 'http://localhost:5235/api/',
+    API_Base_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5235/api/',
 };
 
 export default config;
