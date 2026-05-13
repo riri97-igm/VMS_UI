98364e041dcf85878e2076699ff76d9f6925912e
Igm-16497 <ingyinmay16497@gmail.com>
2026-05-13 22:53:05 +0800

    resolve merge conflict

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,5 +1,10 @@
 const config = {
+<<<<<<< HEAD
     API_Base_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5235/api/',
+=======
+    
+    API_Base_URL: 'http://localhost:5235/api/',
+>>>>>>> 0c97e13 (add password toogle)
 };
 
 export default config;
