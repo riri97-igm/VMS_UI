4c971318fff72a298d80581d0697a32d8248aaa6
Yway Chit Aung <ywaychitaung@gmail.com>
2026-05-14 18:36:18 +0800

    Fix merge conflict in config.jsx

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,10 +1,6 @@
 const config = {
-<<<<<<< HEAD
-    API_Base_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5235/api/',
-=======
-    
-    API_Base_URL: 'http://localhost:5235/api/',
->>>>>>> 0c97e13 (add password toogle)
+  API_Base_URL:
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:5235/api/",
 };
 
 export default config;
